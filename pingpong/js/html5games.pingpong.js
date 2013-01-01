05e699f0326f53c8e60ecbf0b51250cd6e5d643f
John Whish <john.whish@gmail.com>
2013-01-01 22:28:06 +0000

    improved collision detection

--- pingpong/js/html5games.pingpong.js
+++ pingpong/js/html5games.pingpong.js
@@ -10,18 +10,27 @@ var KEY = {
     S: 83
 };
 
-var pingpong = {};
-pingpong.pressedKeys = [];
-pingpong.ball = {
-    speed: 5,
-    x: 150,
-    y: 100,
-    directionX: 1,
-    directionY: 1
+var pingpong;
+pingpong = {
+    scoreA:0, // score for player A
+    scoreB:0, // score for player B
+    pressedKeys:[],
+    ball:{
+        speed:1,
+        x:150,
+        y:100,
+        directionX:1,
+        directionY:1,
+        diameter: 20
+    }
 };
 
 // element selectors
-var $playground, $paddleA, $paddleB, $ball;
+var $playground, $paddleA, $paddleB, $ball, $scoreA, $scoreB;
+
+/*
+ * Start functionality
+ */
 
 function moveBall() {
     "use strict";
@@ -29,29 +38,32 @@ function moveBall() {
     var playgroundHeight = parseInt($playground.height(),10),
         playgroundWidth = parseInt($playground.width(),10),
         ball = pingpong.ball,
+    // TODO: doesn't need to calculated here as X is fixed
         paddleAX = parseInt($paddleA.css("left"),10)+parseInt($paddleA.css("width"),10),
         paddleAYBottom = parseInt($paddleA.css("top"),10)+parseInt($paddleA.css("height"),10),
         paddleAYTop = parseInt($paddleA.css("top"),10),
+    // TODO: doesn't need to calculated here as X is fixed
         paddleBX = parseInt($paddleB.css("left"),10),
         paddleBYBottom = parseInt($paddleB.css("top"),10)+parseInt($paddleB.css("height"),10),
-        paddleBYTop = parseInt($paddleB.css("top"),10);
+        paddleBYTop = parseInt($paddleB.css("top"),10)
 
-// check playground boundary
-// check bottom edge
-    if (ball.y + ball.speed*ball.directionY > playgroundHeight)
-    {
+    // check playground boundary
+
+    // check bottom edge
+    if (ball.y + ball.diameter + ball.speed*ball.directionY > playgroundHeight){
         ball.directionY = -1;
     }
-// check top edge
-    if (ball.y + ball.speed*ball.directionY < 0)
-    {
+    // check top edge
+    if (ball.y + ball.speed*ball.directionY < 0){
         ball.directionY = 1;
     }
-// check right edge
-    if (ball.x +ball.speed*ball.directionX > playgroundWidth)
-    {
-// player B lost.
-// reset the ball;
+
+    // check right edge
+    if (ball.x +ball.speed*ball.directionX > playgroundWidth){
+        // player B lost.
+        pingpong.scoreA += 1;
+        $scoreA.html(pingpong.scoreA);
+        // reset the ball;
         ball.x = 250;
         ball.y = 100;
         $ball.css({
@@ -60,11 +72,12 @@ function moveBall() {
         });
         ball.directionX = -1;
     }
-// check left edge
-    if (ball.x + ball.speed*ball.directionX < 0)
-    {
-// player A lost.
-// reset the ball;
+    // check left edge
+    if (ball.x + ball.speed*ball.directionX < 0){
+        // player A lost.
+        pingpong.scoreB += 1;
+        $scoreB.html(pingpong.scoreB);
+        // reset the ball;
         ball.x = 150;
         ball.y = 100;
         $ball.css({
@@ -75,25 +88,19 @@ function moveBall() {
     }
     ball.x += ball.speed * ball.directionX;
     ball.y += ball.speed * ball.directionY;
-// check moving paddle here
 
     // check left paddle
-
-    if (ball.x + ball.speed*ball.directionX < paddleAX)
-    {
+    if (ball.x + ball.speed*ball.directionX < paddleAX){
         if (ball.y + ball.speed*ball.directionY <= paddleAYBottom &&
-            ball.y + ball.speed*ball.directionY >= paddleAYTop)
-        {
+            ball.y + ball.speed*ball.directionY >= paddleAYTop){
             ball.directionX = 1;
         }
     }
-// check right paddle
 
-    if (ball.x + ball.speed*ball.directionX >= paddleBX)
-    {
+    // check right paddle
+    if (ball.x + ball.diameter + ball.speed*ball.directionX >= paddleBX){
         if (ball.y + ball.speed*ball.directionY <= paddleBYBottom &&
-            ball.y + ball.speed*ball.directionY >= paddleBYTop)
-        {
+            ball.y + ball.speed*ball.directionY >= paddleBYTop){
             ball.directionX = -1;
         }
     }
@@ -108,27 +115,30 @@ function moveBall() {
 function movePaddles() {
     "use strict";
 
-    var top;
+    var top, shift = 2;
     // use our custom timer to continuously check if a key is pressed.
+
+    // Right paddle
     if (pingpong.pressedKeys[KEY.UP]) { // arrow-up
-        // move the paddle B up 5 pixels
+        // move the paddle B up pixels
         top = parseInt($paddleB.css("top"),10);
-        $paddleB.css("top",top-5);
+        $paddleB.css("top",top-shift);
     }
-    if (pingpong.pressedKeys[KEY.DOWN]) { // arrow-down
+    else if (pingpong.pressedKeys[KEY.DOWN]) { // arrow-down
         // move the paddle B down 5 pixels
         top = parseInt($paddleB.css("top"),10);
-        $paddleB.css("top",top+5);
+        $paddleB.css("top",top+shift);
     }
+    // Left paddle
     if (pingpong.pressedKeys[KEY.W]) { // w
         // move the paddle A up 5 pixels
         top = parseInt($paddleA.css("top"),10);
-        $paddleA.css("top",top-5);
+        $paddleA.css("top",top-shift);
     }
-    if (pingpong.pressedKeys[KEY.S]) { // s
+    else if (pingpong.pressedKeys[KEY.S]) { // s
         // move the paddle A down 5 pixels
         top = parseInt($paddleA.css("top"),10);
-        $paddleA.css("top",top+5);
+        $paddleA.css("top",top+shift);
     }
 }
 
@@ -147,7 +157,13 @@ jQuery(function($){
     $paddleA = $('#paddleA');
     $paddleB = $('#paddleB');
     $ball = $('#ball');
+    $scoreA = $('#scoreA');
+    $scoreB = $('#scoreB');
+
     var top;
+
+    // give ball dimensions
+    $ball.width(pingpong.ball.diameter).height(pingpong.ball.diameter);
 
     // set interval to call gameloop every 30 milliseconds
     pingpong.timer = setInterval(gameloop,30);
